70f1c24902e87c98c1b996b34f0b1f32283a81fb
Geoff <geoffreybelcher@icloud.com>
2024-04-23 17:18:00 -0400

    Small updates

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -15,6 +15,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.editor.aerial" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
+    { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.python" },
     { import = "lazyvim.plugins.extras.lang.rust" },
